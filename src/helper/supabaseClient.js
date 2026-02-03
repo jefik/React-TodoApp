0cbe720ff5fb83a49641a851c3e33b6239678205
jefik <jansilak@seznam.cz>
2026-02-03 02:04:33 +0100

    refactor: replace hardcoded key with env

--- src/helper/supabaseClient.js
+++ src/helper/supabaseClient.js
@@ -2,9 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://wuognqodqynqxgmmhmav.supabase.co";
 
-// DONT FORGET TO ADD .ENV FILE AND MAKE THIS ANON
-const supabaseAnonymKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1b2ducW9kcXlucXhnbW1obWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4OTA1NzksImV4cCI6MjA4MTQ2NjU3OX0.XCz57604NE4VNXSTM4G3kzNyQgoF0JKnv7xpFbjuGJc";
+const supabaseAnonymKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonymKey);
 
